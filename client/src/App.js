c16706e398234c5df6fedd5c884532716af75fb6
GOKULKRISHNA-R <88871438+GOKULKRISHNA-R@users.noreply.github.com>
2023-08-21 11:40:18 +0530

    Reset

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,8 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home';
-import Cart from './Cart';
-import Profile from './Profile';
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Cart' element={<Cart/>}/>
-        <Route path='/Profile' element={<Profile/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      
+    </div>
   );
 }
 
--- client/src/components/cartbilldesk.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function CartBillDesk() {
-  return (
-    <div>CartBillDesk</div>
-  )
-}
